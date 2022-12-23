463de21e9117c900340da58abdf37f385790773a
Nikolay K <113373360+Ne1ce@users.noreply.github.com>
2022-12-23 20:54:36 +0300

    version 1.0

---
+++ src/main/java/web/models/Car.java
@@ -0,0 +1,46 @@
+package web.models;
+
+public class Car {
+    private String model;
+    private String country;
+    private int series;
+
+    public Car(String model, String country, int series) {
+        this.model = model;
+        this.country = country;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car " +
+                "model='" + model + '\'' +
+                ", series='" + series + '\'' +
+                ", country=" + country +
+                '.';
+    }
+}
