783d431325aaa02f3b57e010f1d0bce058f91d12
Nikolay K <113373360+Ne1ce@users.noreply.github.com>
2022-12-27 09:58:43 +0300

    1

---
+++ src/main/java/web/models/User.java
@@ -0,0 +1,71 @@
+package web.models;
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class User {
+
+    private int id;
+    @NotEmpty(message = "Empty name")
+    @Size(max =20,message = "To long name")
+    private String name;
+    @NotEmpty(message = "Empty surName")
+    @Size(max =20,message = "To long surName")
+    private String surName;
+    @NotEmpty(message = "Empty email")
+    @Email(message = "invalid email")
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String surName, String email) {
+        this.name = name;
+        this.surName = surName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
